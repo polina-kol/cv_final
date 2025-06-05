87b4845f27b81b44e7da17b9bacafd72b9e0f2d6
polina-kol <kolokolovapolina3@gmail.com>
2025-06-05 18:02:32 +0300

    d

--- utils/brain_utils.py
+++ utils/brain_utils.py
@@ -23,7 +23,7 @@ slice_classes = ["axial", "coronal", "sagittal"]
 classification_transform = transforms.Compose([
     transforms.Resize((224, 224)),  # оптимальный размер для EfficientNet-B0
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+    transforms.Normalize(mean=[0.5], std=[0.5])
 ])
 
 
